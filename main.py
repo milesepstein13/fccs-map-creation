ae1168489c493060b1e98d5089d9c86f4de93eb1
Miles Epstein <miles.epstein13@gmail.com>
2022-06-09 17:38:02 -0700

    added fccs2, trying things in main

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+from cmath import nan
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+import pandas as pd
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import scipy
+import rasterio
+from affine import Affine
+
 # import Canadian fuel data
+canadian_file_path = "data/nat_fbpfuels_2014b.tif"
+cdata = xr.open_rasterio(canadian_file_path)
 
 # convert Canadian fuel types to American
 
 # import American fuel data
+adata = xr.open_dataset("data/fccs_fuelload.nc")
 
 # get both datasets in a form that can be combined
-
+#transform = Affine.from_gdal(*cdata.attrs['transform'])
+#nx, ny = cdata.sizes['x'], cdata.sizes['y']
+#x, y = np.meshgrid(np.arange(nx)+0.5, np.arange(ny)+0.5) * transform
+#print(x)
 # combine datasets
+
+print(adata.ROW)
+
 
 # export to netCDF
--- test.py
+++ test.py
@@ -11,24 +11,24 @@ import rasterio
 from affine import Affine
 
 nc = Dataset('data/fccs_fuelload.nc')
-print("netCDF4: ")
-print(nc)
+#print("netCDF4: ")
+#print(nc)
 data = xr.open_dataset("data/fccs_fuelload.nc")
 print("XARRAY: ")
 print(data)
 print(data.FCCS_FuelLoading)
 dataplot = data.FCCS_FuelLoading.where(data.FCCS_FuelLoading != -9999.)
-dataplot.plot()
-plt.savefig('UStestfig.png')
+#dataplot.plot()
+#plt.savefig('UStestfig.png')
 
 cdata = xr.open_rasterio("data/nat_fbpfuels_2014b.tif")
 percent = np.count_nonzero(cdata)/np.size(cdata)
-print(percent)
+#print(percent)
 print(cdata)
-print("Point:")
-print(cdata[0, 1000, 10001].data)
-cdata[0, 13000:14000, 2000:2500].plot() # full size doesn't resolve, too big
-plt.savefig("CAtestfig.png")
+#print("Point:")
+#print(cdata[0, 1000, 10001].data)
+#cdata[0, 13000:14000, 2000:2500].plot() # full size doesn't resolve, too big
+#plt.savefig("CAtestfig.png")
 
 
 
