e2834773f6c709f0338caaa955db350ce929e816
Miles Epstein <miles.epstein13@gmail.com>
2022-06-10 14:24:13 -0700

    tried reprojecting Canada

--- main.py
+++ main.py
@@ -9,24 +9,48 @@ import cartopy.feature as cfeature
 import scipy
 import rasterio
 from affine import Affine
+import pyproj
+import proj
 
 # import Canadian fuel data
 canadian_file_path = "data/nat_fbpfuels_2014b.tif"
 cdata = xr.open_rasterio(canadian_file_path)
 
+# transform to match US
+print(cdata)
+p1 = pyproj.Proj(cdata.crs)
+#todo: automate parameters below from adata
+p2 = pyproj.Proj("+proj=lcc +lat_0=40 +lon_0=-100 +lat_1=33 +lat_2=45")
+(newx, newy) = pyproj.transform(p1, p2, cdata.x[0:np.size(cdata.y)], cdata.y) #wrong but for proof of concept
+print(newx)
+print(newy)
+
 # convert Canadian fuel types to American
 
+
+print("A")
 # import American fuel data
 adata = xr.open_dataset("data/fccs_fuelload.nc")
+#print(adata.attrs)
 
-# get both datasets in a form that can be combined
-#transform = Affine.from_gdal(*cdata.attrs['transform'])
-#nx, ny = cdata.sizes['x'], cdata.sizes['y']
-#x, y = np.meshgrid(np.arange(nx)+0.5, np.arange(ny)+0.5) * transform
-#print(x)
+# add coordinates from metadata
+xcoords = np.linspace(adata.XORIG, adata.XORIG + adata.XCELL*adata.NCOLS, num=adata.NCOLS)
+ycoords = np.linspace(adata.YORIG, adata.YORIG + adata.YCELL*adata.NROWS, num=adata.NROWS)
+
+adata = adata.FCCS_FuelLoading
+#print(adata)
+adata = adata.assign_coords(COL=xcoords)
+adata = adata.assign_coords(ROW=ycoords)
+#print(adata)
+
+print("FUELLOADING: ")
+#print(adata)
+adata.plot()
+plt.savefig('UStestfig.png')
+
 # combine datasets
 
-print(adata.ROW)
+
 
 
 # export to netCDF
